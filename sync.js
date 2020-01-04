9e8b64ad59b7cbe72ecdd9750dd313db8f9e74be
Thuy Le <gitnap@mailbox.org>
2020-01-04 18:23:03 +0100

    Clean up and refactor sync.js

--- sync.js
+++ sync.js
@@ -3,16 +3,12 @@
 // Node built-in modules
 const path = require('path');
 
-// This module extends the native fs with promise support; most methods are async by default
-const fs = require('fs-extra');
-const shell = require('shelljs');
-
-// const find = require('./find');
-const findAndSave = require('./find-and-save-with-shell');
+const fs = require('fs-extra'); // Extends native fs api w/ promise support, most methods are async
+const shell = require('shelljs'); // Unix shell command in js
 
 // SRC + DEST folders from original-files > copied-files
-const originalDir = path.join(__dirname + "/original-files");
-const copiedDir = path.join(__dirname + "/copied-files");
+const originalDir = path.join("original-files");
+const copiedDir = path.join("copied-files");
 
 const processReadAndCopy = (srcPath, destPath) => {
   fs.readdir(srcPath, (err, items) => {
@@ -23,8 +19,12 @@ const processReadAndCopy = (srcPath, destPath) => {
 }
 
 const copyFiles = (srcPath, destPath, items) => {
-  // Clean up before copy in case of cluttering files
-  if (destPath) shell.rm('-rf', './copied-files');
+  // Clean up before copy
+  if (destPath) {
+    console.log(`Removing copied-files dir....`);
+
+    shell.rm('-rf', './copied-files');
+  }
 
   items.map(item => {
     const fullSrcPath = srcPath + '/' + item;
