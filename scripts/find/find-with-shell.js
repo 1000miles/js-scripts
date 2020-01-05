0ddec91b5ca4f0b673f0f927fecccb60cafccaa1
Thuy Le <gitnap@mailbox.org>
2020-01-05 12:47:06 +0100

    Fix: Differentiate between err and stderr

--- scripts/find/find-with-shell.js
+++ scripts/find/find-with-shell.js
@@ -1,18 +1,20 @@
 #! /usr/bin/env node
 
-// Node buil-in modules
+// Node built-in modules
 const util = require('util');
+// https://nodejs.org/docs/latest-v12.x/api/child_process.html#child_process_child_process_exec_command_options_callback
 const exec = util.promisify(require('child_process').exec);
 
 const findAllOnly = async () => {
-  // Find all files and folders in current dir and count lines of words of output
-  // Store the output of find . (stdout) in data var and print out data in terminal
-  await exec('find . && find . | wc -l', (stdout, stderr) => {
-    if (stderr) return console.error(`exec error: ${stderr}`);
+  // Find all files and folders in current dir and count lines of words
+  await exec('find . && find . | wc -l', (err, stdout, stderr) => {
+    if (err) return console.error(`exec error: ${err}`);
 
+    // Store the output of find . (stdout) in data var
     data = stdout;
 
     console.log(`stdout:`, stdout);
+    console.error(`stderr: ${stderr}`);
   });
 
   // Wait for data to be assembled before printout to terminal
